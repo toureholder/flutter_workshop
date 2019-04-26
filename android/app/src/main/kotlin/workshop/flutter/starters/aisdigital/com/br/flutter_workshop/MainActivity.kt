b08ab489bb82f78851f22bb607435af10316b5c2
Touré Holder <toureholder@gmail.com>
2019-04-26 12:37:20 -0300

    :bulb: Create new flutter project

---
+++ android/app/src/main/kotlin/workshop/flutter/starters/aisdigital/com/br/flutter_workshop/MainActivity.kt
@@ -0,0 +1,13 @@
+package workshop.flutter.starters.aisdigital.com.br.flutter_workshop
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
