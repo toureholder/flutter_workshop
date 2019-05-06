0fea379fc075ef00395181cd7694e992fc56433b
Touré Holder <toureholder@gmail.com>
2019-05-05 22:16:13 -0300

    :repeat: Improve home screen ui

--- lib/model/donation/donation_api.dart
+++ lib/model/donation/donation_api.dart
@@ -6,7 +6,7 @@ import 'package:flutter_workshop/model/donation/donation_api_response.dart';
 
 class DonationApi extends BaseApi {
   Future<List<Donation>> getDonations() async {
-    final url = '${baseUrl}listings/categories/27';
+    final url = '${baseUrl}listings/categories/33';
     final response = await get(url);
     final json = jsonDecode(response.body);
     final DonationApiResponse dto = DonationApiResponse.fromJson(json);
