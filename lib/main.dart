018eafabc65c8fb72bf0d2d58a4870eb7ae3d0a4
Touré Holder <toureholder@gmail.com>
2019-05-06 08:33:14 -0300

    :repeat: Hide debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       localizationsDelegates: [
         const StringLocalizationsDelegate(),
         GlobalMaterialLocalizations.delegate,
