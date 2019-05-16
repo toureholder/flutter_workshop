afcd27702d98ea96dbcc9b42dc99214e46f9b7c1
Touré Holder <toureholder@gmail.com>
2019-05-16 05:11:42 -0300

    :cool: Remove form validation from UI

---
+++ lib/util/form_field_validator.dart
@@ -0,0 +1,25 @@
+class CustomFormFieldValidator {
+  static String validateEmail(String input) {
+    String message;
+
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = new RegExp(pattern);
+
+    if (input.trim().isEmpty)
+      message = 'validation_message_email_required';
+    else if (!regex.hasMatch(input))
+      message = 'validation_message_email_invalid';
+    return message;
+  }
+
+  static String validatePassword(String input) {
+    String message;
+
+    if (input.trim().isEmpty)
+      message = 'validation_message_password_required_test';
+    else if (input.length < 6)
+      message = 'validation_message_password_too_short';
+    return message;
+  }
+}
