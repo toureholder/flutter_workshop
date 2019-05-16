175073e26aa177be1ced6b3c410fb2ee6fcdc1de
Touré Holder <toureholder@gmail.com>
2019-05-16 05:33:56 -0300

    :white_check_mark: Unit test login from validators

---
+++ test/custom_form_field_validator_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_workshop/util/custom_form_field_validator.dart';
+import 'package:test_api/test_api.dart';
+
+void main() {
+  test('empty email returns error string', () {
+    final result = CustomFormFieldValidator.validateEmail('');
+    expect(result, 'validation_message_email_required');
+  });
+
+  test('invalid email returns error string', () {
+    final result = CustomFormFieldValidator.validateEmail('invalid email');
+    expect(result, 'validation_message_email_invalid');
+  });
+
+  test('empty password returns error string', () {
+    final result = CustomFormFieldValidator.validatePassword('');
+    expect(result, 'validation_message_password_required');
+  });
+
+  test('short password returns error string', () {
+    final result = CustomFormFieldValidator.validatePassword('12345');
+    expect(result, 'validation_message_password_too_short');
+  });
+}
