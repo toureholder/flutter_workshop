78ade86a488ce63bca1732ad27a017e8b3c5717e
Touré Holder <toureholder@gmail.com>
2019-05-25 18:28:55 -0300

    :white_check_mark: Test Home widget

---
+++ test/home_test.dart
@@ -0,0 +1,55 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_workshop/base/dependency_provider.dart';
+import 'package:flutter_workshop/feature/home/home.dart';
+import 'package:flutter_workshop/feature/home/home_bloc.dart';
+import 'package:flutter_workshop/model/donation/donation.dart';
+import 'package:mockito/mockito.dart';
+import 'package:image_test_utils/image_test_utils.dart';
+
+import 'test_util/test_util.dart';
+
+class MockHomeBloc extends Mock implements HomeBloc {}
+
+void main() {
+  final _mockHomeBloc = MockHomeBloc();
+
+  final testableWidget = TestUtil.makeTestableWidget(
+      subject: Home(), dependencies: AppDependencies(homeBloc: _mockHomeBloc));
+
+  testWidgets('shows circular progress inidicator while loading',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(testableWidget);
+
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+
+  testWidgets('loads donations', (WidgetTester tester) async {
+    await tester.pumpWidget(testableWidget);
+
+    verify(_mockHomeBloc.loadDonations());
+  });
+
+  testWidgets('shows list when donations are added to stream',
+      (WidgetTester tester) async {
+
+    provideMockedNetworkImages(() async {
+      final controller = StreamController<List<Donation>>.broadcast();
+
+      when(_mockHomeBloc.stream).thenAnswer((_) => controller.stream);
+
+      await tester.pumpWidget(testableWidget);
+
+      controller.sink.add(Donation.fakeList());
+
+      await tester.pump(Duration.zero);
+
+      expect(find.byType(CircularProgressIndicator), findsNothing);
+      expect(find.byType(ListView), findsOneWidget);
+
+      controller.close();
+    });
+  });
+}
