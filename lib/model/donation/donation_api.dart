3d3150230fe0cfc4572359cfdc2c88c8c265cb76
Touré Holder <toureholder@gmail.com>
2019-07-06 21:16:36 -0300

    :racehorse: Manually spawn isolate to parse json

--- lib/model/donation/donation_api.dart
+++ lib/model/donation/donation_api.dart
@@ -1,9 +1,12 @@
 import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'dart:isolate';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter_workshop/base/base_api.dart';
 import 'package:flutter_workshop/model/donation/donation.dart';
 import 'package:flutter_workshop/model/donation/donation_api_response.dart';
+import 'package:flutter_workshop/util/concurrency.dart';
+import 'package:http/http.dart' as http;
 import 'package:meta/meta.dart';
 
 class DonationApi extends BaseApi {
@@ -13,7 +16,23 @@ class DonationApi extends BaseApi {
     final String url = '${baseUrl}listings/categories/33';
     final http.Response response = await get(url);
     final Map<String, dynamic> json = jsonDecode(response.body);
-    final DonationApiResponse dto = DonationApiResponse.fromJson(json);
+    final dto =
+        await computeInIsolate<DonationApiResponse>(parseDonations, json);
     return dto.donations;
+  }
+}
+
+void parseDonations(SendPort sendPort) async {
+  final port = ReceivePort();
+  sendPort.send(port.sendPort);
+
+  await for (var message in port) {
+    if (message is RespondableMessage) {
+      final json = message.data;
+      final result = DonationApiResponse.fromJson(json);
+      message.sendPort.send(result);
+    }
+
+    port.close();
   }
 }
