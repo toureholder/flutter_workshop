f42e14c3d396f01f1cb34c295ffd15da0e4f5294
Touré Holder <toureholder@gmail.com>
2019-07-06 21:20:01 -0300

    :cool: Parse json in a seperate isolate with compute

--- lib/model/donation/donation_api.dart
+++ lib/model/donation/donation_api.dart
@@ -1,13 +1,14 @@
 import 'dart:convert';
-import 'dart:isolate';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter_workshop/base/base_api.dart';
 import 'package:flutter_workshop/model/donation/donation.dart';
 import 'package:flutter_workshop/model/donation/donation_api_response.dart';
-import 'package:flutter_workshop/util/concurrency.dart';
 import 'package:http/http.dart' as http;
 import 'package:meta/meta.dart';
+
+DonationApiResponse parseDonations(Map<String, dynamic> json) =>
+    DonationApiResponse.fromJson(json);
 
 class DonationApi extends BaseApi {
   DonationApi({@required http.Client client}) : super(client: client);
@@ -16,23 +17,7 @@ class DonationApi extends BaseApi {
     final String url = '${baseUrl}listings/categories/33';
     final http.Response response = await get(url);
     final Map<String, dynamic> json = jsonDecode(response.body);
-    final dto =
-        await computeInIsolate<DonationApiResponse>(parseDonations, json);
+    final DonationApiResponse dto = await compute(parseDonations, json);
     return dto.donations;
-  }
-}
-
-void parseDonations(SendPort sendPort) async {
-  final port = ReceivePort();
-  sendPort.send(port.sendPort);
-
-  await for (var message in port) {
-    if (message is RespondableMessage) {
-      final json = message.data;
-      final result = DonationApiResponse.fromJson(json);
-      message.sendPort.send(result);
-    }
-
-    port.close();
   }
 }
