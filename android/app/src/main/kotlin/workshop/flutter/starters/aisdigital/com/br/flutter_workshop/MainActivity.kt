9580331dd20503ea1a59d785884b76109b94ab2e
Touré Holder <toureholder@gmail.com>
2021-04-27 23:48:34 -0300

    :wrench: migrate to Android V2 Embedding

--- android/app/src/main/kotlin/workshop/flutter/starters/aisdigital/com/br/flutter_workshop/MainActivity.kt
+++ android/app/src/main/kotlin/workshop/flutter/starters/aisdigital/com/br/flutter_workshop/MainActivity.kt
@@ -1,13 +1,6 @@
 package workshop.flutter.starters.aisdigital.com.br.flutter_workshop
 
-import android.os.Bundle
-
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity;
 
 class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
 }
